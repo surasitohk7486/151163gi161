3109437a8436f4dd8cd0713844c1d75fb7e4c11d
Nangil <surasit.dang@bumail.net>
2023-11-15 12:59:03 +0700

    Finish Ball Physics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BallColor
+{
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+   }
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private int point;
+    public int Point { get { return point} }
+
+    [SerializeField]
+    private BallColor color;
+
+    [SerializeField]
+    private MeshRenderer rd;
+    
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+    public void SetColor(BallColor color)
+    {
+        switch (color)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown: 
+                point = 4;
+                rd.material.color = new Color32(212,53,17,255);
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = new Color32(255,98,193,255);
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = new Color32(255,154,240,255);
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+
+        }
+    }
+   
+}
